61e298e08b865ec8c7220fa1cffe78b4ee8c19d9
Seiron92 <42301363+Seiron92@users.noreply.github.com>
2019-10-07 12:58:43 +0200

    Updated with error msg

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,18 +1,16 @@
-const urlGet = "https://studenter.miun.se/~rese1800/dt173g/webbtjanstDel1/corses.php/api";
+const urlGet = "http://studenter.miun.se/~rese1800/dt173g/webbtjanstDel1/corses.php/api";
 fetch(urlGet).then(function (response) {
   if (response.status !== 200) {
     console.log(response.status);
     return;
   }
-  /* IF STATUS 200 SHOW DATA */
-
 
   response.json().then(function (data) {
     data.forEach(element => {
       $("#courses").append('<tr><td>' + element.Code + '</td><td class="long">' + element.Name + '</td><td class="center">' + element.Progression + '</td><td><a href="' + element.Course_syllabus + '" target="blank">Webblänk</a></td><td class="center btn" id="deletebtn' + element.Id + '">x</td><td class="btn" id="btn' + element.Id + '">Ändra</td></tr>');
       const change = $('#btn' + element.Id);
       const deletebtn = $('#deletebtn' + element.Id);
-      const urlSet = "https://studenter.miun.se/~rese1800/dt173g/webbtjanstDel1/corses.php/api/" + element.Id;
+      const urlSet = "http://studenter.miun.se/~rese1800/dt173g/webbtjanstDel1/corses.php/api/" + element.Id;
       /* CHANGE COURSE */
 
       $(change).click(function () {
@@ -20,25 +18,39 @@ fetch(urlGet).then(function (response) {
           scrollTop: $("#newname").offset().top
         }, 2000);
         $('#newname').html('Ändra kurs');
-        $('#onchange').html('<button id="submit2">Ändra</button>');
+        $('#submit').html('Ändra');
         $('#ccode').val(element.Code);
         $('#cname').val(element.Name);
         $('#prog').val(element.Progression);
         $('#sylla').val(element.Course_syllabus);
-        $('#submit2').click(function () {
-          let jsonStr = JSON.stringify({
-            "code": $('#ccode').val(),
-            "name": $('#cname').val(),
-            "prog": $('#prog').val(),
-            "course_syllabus": $('#sylla').val()
-          });
-          fetch(urlSet, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: jsonStr
-          }).then(res => res.json()).then(data => location.reload()).catch(err => console.log(err));
+        $('#submit').click(function () {
+          if ($('#ccode').val() == '') {
+            $('#ccode').focus();
+            $('#error').html('Alla fält måste vara ifyllda');
+          } else if ($('#cname').val() == '') {
+            $('#cname').focus();
+            $('#error').html('Alla fält måste vara ifyllda');
+          } else if ($('#prog').val() == '') {
+            $('#prog').focus();
+            $('#error').html('Alla fält måste vara ifyllda');
+          } else if ($('#sylla').val() == '') {
+            $('#sylla').focus();
+            $('#error').html('Alla fält måste vara ifyllda');
+          } else {
+            let jsonStr = JSON.stringify({
+              "code": $('#ccode').val(),
+              "name": $('#cname').val(),
+              "prog": $('#prog').val(),
+              "course_syllabus": $('#sylla').val()
+            });
+            fetch(urlSet, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: jsonStr
+            }).then(res => res.json()).then(data => location.reload()).catch(err => console.log(err));
+          }
         });
       });
       /* DELETE COURSE */
@@ -55,19 +67,33 @@ fetch(urlGet).then(function (response) {
     /* SUBMIT COURSE */
 
     $('#submit').click(function () {
-      let jsonStr = JSON.stringify({
-        "code": $('#ccode').val(),
-        "name": $('#cname').val(),
-        "prog": $('#prog').val(),
-        "course_syllabus": $('#sylla').val()
-      });
-      fetch(urlGet, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: jsonStr
-      }).then(res => res.json()).then(data => location.reload()).catch(err => console.log(err));
+      if ($('#ccode').val() == '') {
+        $('#ccode').focus();
+        $('#error').html('Alla fält måste vara ifyllda');
+      } else if ($('#cname').val() == '') {
+        $('#cname').focus();
+        $('#error').html('Alla fält måste vara ifyllda');
+      } else if ($('#prog').val() == '') {
+        $('#prog').focus();
+        $('#error').html('Alla fält måste vara ifyllda');
+      } else if ($('#sylla').val() == '') {
+        $('#sylla').focus();
+        $('#error').html('Alla fält måste vara ifyllda');
+      } else {
+        let jsonStr = JSON.stringify({
+          "code": $('#ccode').val(),
+          "name": $('#cname').val(),
+          "prog": $('#prog').val(),
+          "course_syllabus": $('#sylla').val()
+        });
+        fetch(urlGet, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: jsonStr
+        }).then(res => res.json()).then(data => location.reload()).catch(err => console.log(err));
+      }
     });
   });
 }).catch(function (err) {
